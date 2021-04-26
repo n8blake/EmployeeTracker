41fbead937a557a3b40d9d13ec233c748f2d04fd
Nate <n8blake@mac.com>
2021-04-26 11:12:55 -0700

    finish department mysql tests

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const sequelize = require('./config/db.js');
+const inquirer = require('inquirer');
+
 
 const Employee = require('./models/Employee');
 const Department = require('./models/Department');
@@ -7,4 +9,8 @@ const Role = require('./models/Role');
 
 sequelize.sync().then(() => {
 	console.log('Connected to DB.');
+	// let dept = Department.create({
+	// 	name: 'My Department'
+	// });
+	// console.log(dept);
 });
